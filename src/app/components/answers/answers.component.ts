c6b54cee0a572efb758e49a036838a573d970b35
bartlomiejwyszecki <bartlomiej.wyszecki2@gmail.com>
2021-02-12 18:24:14 +0100

    Added components, application is already working but without async and observable data service.

--- src/app/components/answers/answers.component.ts
+++ src/app/components/answers/answers.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { WordType } from 'src/app/data/models';
+import { WordsService } from 'src/app/services/words.service';
 
 @Component({
   selector: 'app-answers',
@@ -6,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./answers.component.css']
 })
 export class AnswersComponent implements OnInit {
+
+  @Input() title: string;
+  @Input() words: WordType[];
 
   constructor() { }
 
